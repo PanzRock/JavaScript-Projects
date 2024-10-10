07e2bdb650b4d9e3141ff6d7d845819b04892103
PanzRock <pancholbato08@gmail.com>
2024-10-10 00:42:33 -0600

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alerts/JS/main.js
@@ -0,0 +1,14 @@
+window.alert('Welcome to my game!')
+document.write('This is the best MMORPG game in the whole wide world!\<br>')
+var A = "World of Fate";
+document.write(A);
+document.write('Once upon a time in the land of Maydorm, an evil sorcerrer wanted to conquer the land and bring darkness to it, but a warrior, name \"Atlas"\ fight for the survival of the land.\<br>\With his training with the shaolin and monks, he gained the power of the light and used it to fight against the evil sorcerrer.\<br>')
+document.write("\"I shall fight the evil darkness,"
+    + " fight for the sake of my people,"
+    + " rescue them from darkness and bring them to light."
+    + " I don\'t mind sacrificing my life,"
+    + " as long as I can bring end to this darkness by the power of the light.\""
+    + " -Atlas\<br>")
+var Region = "Land of Maydorm", Class1 = "Warrior", Class2 = "Assassin", Class3 = "Wizard", Class4 = "Ranger", Class5 = "Gunner", Class6 = "Necromancer", Class7 = "Druid";
+document.write(Class6)
+document.write(3 + 3);
