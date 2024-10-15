c663bbb91d6b545c1caac27950bb6b938603b406
PanzRock <pancholbato08@gmail.com>
2024-10-15 11:05:11 -0600

    Create main.js

---
+++ Basic JavaScript Projects/Project5_comparison_type_coercion/JS/main.js
@@ -0,0 +1,34 @@
+var x = true; //variable with a Boolean data type
+
+//printing the data type of the variable x
+document.write(typeof x);
+document.write("<br>");
+
+//an expression combining a string and a number
+document.write("10" + 10);
+document.write("<br>");
+
+//utilizing the == operator
+document.write(4 == 4);
+document.write("<br>");
+
+//utilizing the === operator
+x = 5
+y = 5
+document.write(x === y);
+document.write("<br>");
+
+//utilizing the > and the && operators
+document.write(10 > 7 && 12 > 9);
+document.write("<br>");
+
+//utilizing the < and || operators
+document.write(15 < 12 || 20 < 7);
+document/write("<br>");
+
+//utilizing the ! operator
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(4 >8);
+}
+
+
